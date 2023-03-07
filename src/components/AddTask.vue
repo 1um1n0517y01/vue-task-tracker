722a90d997c6ae80aeda7419f3fcfe87ec0ace64
1um1n0517y01 <34325214+1um1n0517y01@users.noreply.github.com>
2023-03-07 23:36:52 +0100

    Implemented Add Task form design

---
+++ src/components/AddTask.vue
@@ -0,0 +1,73 @@
+<template>
+  <form className="add-form">
+    <div className="form-control">
+      <label>Task</label>
+      <input type="text" v-model="text" name="text" placeholder="Add Task" />
+    </div>
+    <div className="form-control">
+      <label>Day & Time</label>
+      <input
+        type="text"
+        v-model="day"
+        name="day"
+        placeholder="Add Day & Time"
+      />
+    </div>
+    <div className="form-control form-control-check">
+      <label>Set reminder</label>
+      <input type="checkbox" v-model="reminder" name="reminder" />
+    </div>
+
+    <input type="submit" class="btn btn-block" value="Save Task" />
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'AddTask',
+  data() {
+    return {
+      text: '',
+      day: '',
+      reminder: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.add-form {
+  margin-bottom: 40px;
+}
+
+.form-control {
+  margin: 20px 0;
+}
+
+.form-control label {
+  display: block;
+}
+
+.form-control input {
+  width: 100%;
+  height: 40px;
+  margin: 5px;
+  padding: 3px 7px;
+  font-size: 17px;
+}
+
+.form-control-check {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.form-control-check label {
+  flex: 1;
+}
+
+.form-control-check input {
+  flex: 2;
+  height: 20px;
+}
+</style>
